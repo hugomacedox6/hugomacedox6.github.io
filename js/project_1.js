495684d892753b4edb571ee6f3288524cd83fe26
hugox6 <89811164+hugox6@users.noreply.github.com>
2021-11-04 11:05:57 -0300

    Add files via upload

---
+++ js/project_1.js
@@ -0,0 +1,18 @@
+function calculate(){
+   binary=document.getElementById("binary").value;
+   binary = parseInt(binary, 2);
+   document.getElementById("decimal").value = binary;
+   
+
+
+
+//variable.toString(2);    // Binary
+//variable.toString(8);   // Octal
+//variable.toString(16);  // Hex
+
+//parseInt(variable, 2);  // Binary to Decimal
+//parseInt(variable, 8);  // Octal to Decimal
+//parseInt(variable, 16); // Hex to Decimal
+
+
+}
